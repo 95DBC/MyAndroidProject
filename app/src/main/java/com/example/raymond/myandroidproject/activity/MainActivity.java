ffa8902115061ad208109b06479069760467ec0a
95DBC <171291124@qq.com>
2018-02-08 18:27:10 +0800

    获取不到图片

--- app/src/main/java/com/example/raymond/myandroidproject/activity/MainActivity.java
+++ app/src/main/java/com/example/raymond/myandroidproject/activity/MainActivity.java
@@ -1,16 +1,43 @@
 package com.example.raymond.myandroidproject.activity;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.ContentUris;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.raymond.myandroidproject.R;
 import com.example.raymond.myandroidproject.widget.PhotoDialog;
+import com.liji.circleimageview.CircleImageView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
     private Context mContext;
+    private PhotoDialog dialog;
+    private Uri imageUri;
+    public static final int TAKE_PHOTO = 1;
+    public static final int CHOOSE_PHOTO = 2;
+    private CircleImageView circleImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +49,176 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                PhotoDialog dialog = new PhotoDialog(mContext,R.style.PhotoDialog);
+                dialog = new PhotoDialog(mContext, R.style.PhotoDialog);
                 dialog.show();
+                Button btn_photoAlbum = dialog.findViewById(R.id.btn_photoAlbum);
+                btn_photoAlbum.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                            ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                            }, 1);
+                        } else {
+                            openAlbum();
+                            dialog.dismiss();
+                        }
+                    }
+                });
+                Button btn_photoCamera = dialog.findViewById(R.id.btn_photoCamera);
+                btn_photoCamera.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+//                创建File 对象，用于存储拍照后的图片
+                        File outputImage = new File(getExternalCacheDir(), "out_image.jpg");
+                        dialog.dismiss();
+                        try {
+                            if (outputImage.exists()) {
+                                outputImage.delete();
+                            }
+                            outputImage.createNewFile();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        if (Build.VERSION.SDK_INT >= 24) {
+// TODO:           这里的第二个参数，需要更具相对应的程序修改对应的额包名
+                            imageUri = FileProvider.getUriForFile(MainActivity.this
+                                    , "com.example.raymond.myandroidproject.fileprovider", outputImage);
+                        } else {
+                            imageUri = Uri.fromFile(outputImage);
+                        }
+                        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                        startActivityForResult(intent, TAKE_PHOTO);
+                    }
+                });
             }
         });
+
     }
+
+    /**
+     * 绑定事件
+     */
+    private void bindChoicePhotoEvent() {
+
+    }
+
+
+
+    /**
+     * 打开相册
+     */
+    private void openAlbum() {
+        Intent intent = new Intent("android.intent.action.GET_CONTENT");
+        intent.setType("image/*");
+        startActivityForResult(intent, CHOOSE_PHOTO);
+    }
+
+    public void onRequestPermissionResult(int requestCode, String[] permission, int[] grantResults) {
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    openAlbum();
+                } else {
+                    Toast.makeText(mContext, "ni shengming le quanxian", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    @SuppressLint("WrongViewCast")
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case TAKE_PHOTO:
+                if (requestCode == RESULT_OK) {
+                    try {
+                        Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
+                        circleImageView = findViewById(R.id.iv_headIcon);
+                        circleImageView.setImageBitmap(bitmap);
+                        if (bitmap== null){
+                            Log.e("bitmap 值为多少", "Bimap 值为空 " );
+                        }
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+                break;
+            case CHOOSE_PHOTO:
+                if (requestCode == RESULT_OK) {
+                    if (resultCode == RESULT_OK) {
+//                        判断手机系统版本
+                        if (Build.VERSION.SDK_INT >= 19) {
+                            handleImageOnKitKat(data);
+                        } else {
+                            handleImageBeforeKitKat(data);
+                        }
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void handleImageOnKitKat(Intent data) {
+        String imagePath = null;
+        Uri uri = data.getData();
+        if (DocumentsContract.isDocumentUri(this, uri)) {
+//            如果是document 类型的Uri,则通过document id 处理
+            String docId = DocumentsContract.getDocumentId(uri);
+            if ("com.android.providers.media.documents".equals(uri.getAuthority())) {
+                String id = docId.split(":")[1];
+                String selection = MediaStore.Images.Media._ID + "=" + id;
+                imagePath = getImagePath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, selection);
+            } else if ("com.android.providers.downloads.documents".equals(uri.getAuthority())) {
+                Uri contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads/public_dwonloads")
+                        , Long.valueOf(docId));
+                imagePath = getImagePath(contentUri, null);
+            } else if ("content".equalsIgnoreCase(uri.getScheme())) {
+//                如果是content 类型的Uri,则使用普通方式处理
+                imagePath = getImagePath(uri, null);
+            } else if ("file".equalsIgnoreCase(uri.getScheme())) {
+                imagePath = uri.getPath();
+            }
+            displayImage(imagePath);
+        }
+
+    }
+
+    private void displayImage(String imagePath) {
+        if (imagePath != null) {
+            Bitmap bitmap = BitmapFactory.decodeFile(imagePath);
+            circleImageView.setImageBitmap(bitmap);
+            if (bitmap== null){
+                Log.e("bitmap 值为多少", "Bimap 值为空 " );
+            }
+        } else {
+            Toast.makeText(mContext, "找不到照片", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private String getImagePath(Uri uri, String selection) {
+        String path = null;
+//        通过Uri 和 selectio 来获取这是的图片路径
+        Cursor cursor = getContentResolver().query(uri, null, selection, null, null);
+        if (cursor != null) {
+            if (cursor.moveToFirst()) {
+                path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+            }
+            cursor.close();
+        }
+        return path;
+    }
+
+    private void handleImageBeforeKitKat(Intent data) {
+        Uri uri = data.getData();
+        String imagePath = getImagePath(uri, null);
+        displayImage(imagePath);
+    }
+
+
 }
--- app/src/main/java/com/example/raymond/myandroidproject/util/ChoicePhotoUtil.java
+++ app/src/main/java/com/example/raymond/myandroidproject/util/ChoicePhotoUtil.java
@@ -1,5 +1,23 @@
 package com.example.raymond.myandroidproject.util;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
+
+import java.io.File;
+import java.io.IOException;
+
+import static android.app.Activity.RESULT_OK;
+import static android.support.v4.app.ActivityCompat.startActivityForResult;
+import static android.support.v4.content.ContextCompat.getExternalCacheDirs;
+import static android.support.v4.content.ContextCompat.startActivity;
+
 /**
  * Created by Raymond 陈徐锋 on 2018/2/7.
  * Email: raymond@hinteen.com
@@ -7,25 +25,54 @@ package com.example.raymond.myandroidproject.util;
  */
 
 public class ChoicePhotoUtil {
+    private Uri imageUri;
+    public static final int TAKE_PHOTO = 1;
 
     /**
      * 从相册中获取
      */
-    private void getByGallery(){
+    private void getByGallery() {
 
     }
 
     /**
      * 启动照相机
      */
-    private void getByCamera(){
+    private void getByCamera(Context context) {
+//        创建File 对象，用于存储拍照后的图片
+        File outputImage = new File(String.valueOf(getExternalCacheDirs(context)), "output_image.jpg");
+        try {
+            if (outputImage.exists()) {
+                outputImage.delete();
+            }
+            outputImage.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (Build.VERSION.SDK_INT >= 24) {
+            // TODO: 2018/2/8 这里的第二参数需要根据对应的程序修改包名
+            imageUri = FileProvider.getUriForFile(context, "com.example.raymond.myandroidproject", outputImage);
+        } else {
+            imageUri = Uri.fromFile(outputImage);
+        }
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+//        startActivityForResult(intent,TAKE_PHOTO);
+    }
 
+    protected void onActivityResult(int requestCode,int resultCode,Intent data){
+        switch (requestCode){
+            case TAKE_PHOTO:
+                if (requestCode == RESULT_OK){
+//                    Bitmap bitmap = BitmapFactory.decodeStream(getCo)
+                }
+        }
     }
 
     /**
      * 裁剪图片
      */
-    private void clipPhoto(){
+    private void clipPhoto() {
 
     }
 
